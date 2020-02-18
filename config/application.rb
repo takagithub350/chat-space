9f8933d32f85be03b3f45c8741af48b00960537b
programing-biginner <60635421+programing-biginner@users.noreply.github.com>
2020-02-18 20:57:06 +0900

    add

--- config/application.rb
+++ config/application.rb
@@ -5,17 +5,18 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.generators do |g|
-  g.stylesheets false
-  g.javascripts false
-  g.helper false
-  g.test_framework false
+
 
 module ChatSpace
   class Application < Rails::Application
-
+   config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
 end
